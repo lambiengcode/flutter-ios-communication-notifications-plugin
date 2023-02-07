d45e86fcd3f75ad8150b6b43eafd6e94c72c4dcb
lambiengcode <lambiengcode@gmail.com>
2023-02-07 12:49:23 +0700

    Bump version 0.0.9

--- ios/Classes/IosCommunicationNotificationPlugin.swift
+++ ios/Classes/IosCommunicationNotificationPlugin.swift
@@ -16,8 +16,10 @@ public class IosCommunicationNotificationPlugin: NSObject, FlutterPlugin {
             let arguments = call.arguments as? [String: Any] ?? [String: Any]()
             let senderName = arguments["senderName"] as? String ?? ""
             let content = arguments["content"] as? String ?? ""
-            guard let avatar = arguments["imageBytes"] as? FlutterStandardTypedData)?.data
-            else { return }
+            guard let avatar = arguments["imageBytes"] as? FlutterStandardTypedData)?.data else {
+                result(false)
+                return
+            }
             let value = arguments["value"] as? String ?? ""
             CommunicationNotificationPlugin().showNotification(NotificationInfo(senderName: senderName, pngImage: avatar, content: content, value: value))
             result(true)
