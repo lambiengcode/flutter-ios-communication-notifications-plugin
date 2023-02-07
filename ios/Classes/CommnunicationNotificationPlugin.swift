55659d7858b112430184098128cbb881973c7cc0
lambiengcode <lambiengcode@gmail.com>
2023-02-07 11:55:33 +0700

    Bump version 0.0.7

--- ios/Classes/CommnunicationNotificationPlugin.swift
+++ ios/Classes/CommnunicationNotificationPlugin.swift
@@ -24,81 +24,83 @@ class CommunicationNotificationPlugin {
     }
     
     func dispatchNotification(_ notificationInfo: NotificationInfo) {
-        let identifier = "mason"
-        var content = UNMutableNotificationContent()
-        
-        content.title = notificationInfo.senderName
-        content.subtitle = ""
-        content.body = notificationInfo.content
-        content.sound = nil
-        content.categoryIdentifier = "Event"
-        
-        var personNameComponents = PersonNameComponents()
-        personNameComponents.nickname = notificationInfo.senderName
-        
-        let avatar = INImage(imageData: UIImage(named: "avatar_default")!.pngData()!)
-        
-        let senderPerson = INPerson(
-            personHandle: INPersonHandle(value: notificationInfo.value, type: .unknown),
-            nameComponents: personNameComponents,
-            displayName: notificationInfo.senderName,
-            image: avatar,
-            contactIdentifier: nil,
-            customIdentifier: nil,
-            isMe: false,
-            suggestionType: .none
-        )
-        
-        let mePerson = INPerson(
-            personHandle: INPersonHandle(value: "", type: .unknown),
-            nameComponents: nil,
-            displayName: nil,
-            image: nil,
-            contactIdentifier: nil,
-            customIdentifier: nil,
-            isMe: true,
-            suggestionType: .none
-        )
-        
-        
-        let intent = INSendMessageIntent(
-            recipients: [mePerson],
-            outgoingMessageType: .outgoingMessageText,
-            content: "Text",
-            speakableGroupName: INSpeakableString(spokenPhrase: "Sender Name"),
-            conversationIdentifier: "sampleConversationIdentifier",
-            serviceName: nil,
-            sender: senderPerson,
-            attachments: nil
-        )
-        
-        intent.setImage(avatar, forParameterNamed: \.sender)
-        
-        let interaction = INInteraction(intent: intent, response: nil)
-        interaction.direction = .incoming
-        
-        interaction.donate(completion: nil)
-        
-        do {
-            content = try content.updating(from: intent) as! UNMutableNotificationContent
-        } catch {
-            // Handle errors
+        if #available(iOS 15.0, *) {
+            let identifier = "mason"
+            var content = UNMutableNotificationContent()
+            
+            content.title = notificationInfo.senderName
+            content.subtitle = ""
+            content.body = notificationInfo.content
+            content.sound = nil
+            content.categoryIdentifier = "Event"
+            
+            var personNameComponents = PersonNameComponents()
+            personNameComponents.nickname = notificationInfo.senderName
+            
+            let avatar = INImage(imageData: UIImage(named: "avatar_default")!.pngData()!)
+            
+            let senderPerson = INPerson(
+                personHandle: INPersonHandle(value: notificationInfo.value, type: .unknown),
+                nameComponents: personNameComponents,
+                displayName: notificationInfo.senderName,
+                image: avatar,
+                contactIdentifier: nil,
+                customIdentifier: nil,
+                isMe: false,
+                suggestionType: .none
+            )
+            
+            let mePerson = INPerson(
+                personHandle: INPersonHandle(value: "", type: .unknown),
+                nameComponents: nil,
+                displayName: nil,
+                image: nil,
+                contactIdentifier: nil,
+                customIdentifier: nil,
+                isMe: true,
+                suggestionType: .none
+            )
+            
+            
+            let intent = INSendMessageIntent(
+                recipients: [mePerson],
+                outgoingMessageType: .outgoingMessageText,
+                content: notificationInfo.content,
+                speakableGroupName: INSpeakableString(spokenPhrase: notificationInfo.senderName),
+                conversationIdentifier: notificationInfo.senderName,
+                serviceName: nil,
+                sender: senderPerson,
+                attachments: nil
+            )
+            
+            intent.setImage(avatar, forParameterNamed: \.sender)
+            
+            let interaction = INInteraction(intent: intent, response: nil)
+            interaction.direction = .incoming
+            
+            interaction.donate(completion: nil)
+            
+            do {
+                content = try content.updating(from: intent) as! UNMutableNotificationContent
+            } catch {
+                // Handle errors
+            }
+            
+            // Show 0 seconds from now
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
+            
+            // Request from identifier
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+            
+            // actions
+            let close = UNNotificationAction(identifier: "close", title: "Close", options: .destructive)
+            //        let reply = UNNotificationAction(identifier: "reply", title: "Reply", options: .foreground)
+            let category = UNNotificationCategory(identifier: "Event", actions: [close], intentIdentifiers: [])
+            
+            UNUserNotificationCenter.current().setNotificationCategories([category])
+            
+            // Add notification request
+            UNUserNotificationCenter.current().add(request)
         }
-        
-        // Show 0 seconds from now
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
-        
-        // Request from identifier
-        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-        
-        // actions
-        let close = UNNotificationAction(identifier: "close", title: "Close", options: .destructive)
-        //        let reply = UNNotificationAction(identifier: "reply", title: "Reply", options: .foreground)
-        let category = UNNotificationCategory(identifier: "Event", actions: [close], intentIdentifiers: [])
-        
-        UNUserNotificationCenter.current().setNotificationCategories([category])
-        
-        // Add notification request
-        UNUserNotificationCenter.current().add(request)
     }
 }
